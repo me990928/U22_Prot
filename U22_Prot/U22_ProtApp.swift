f6e7dff4ac689fe614601cb8804902443399f4f0
nanana <108167232+me990928@users.noreply.github.com>
2023-04-21 13:29:28 +0900

    Initial Commit

---
+++ U22_Prot/U22_ProtApp.swift
@@ -0,0 +1,17 @@
+//
+//  U22_ProtApp.swift
+//  U22_Prot
+//
+//  Created by 広瀬友哉 on 2023/04/21.
+//
+
+import SwiftUI
+
+@main
+struct U22_ProtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
